fb7e1201888e2eee8be94eaedfe8b7eaa52441db
Abram <israelvictory87@gmail.com>
2022-09-01 17:51:35 +0100

    Update Footer component and stylings

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,13 @@ import "./footer_styles.css";
 
 const Footer = () => {
     return (
-        <></>
+        <>
+            <footer className="footer__bg">
+                <p className="design__copyright">
+                    Designed by <a href="#" target="_blank" className="design__link">the UX Guy</a>
+                </p>
+            </footer>
+        </>
     )
 }
 
