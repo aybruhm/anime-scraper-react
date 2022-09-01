6398b0423fdc67c5e51bde71cde8f4edf3ea90cd
Abram <israelvictory87@gmail.com>
2022-09-01 16:58:36 +0100

    Created SearchResult component and stylings

---
+++ src/components/results/SearchResults.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./search_results.css";
+import Navbar from '../navbar/Navbar';
+
+
+const SearchResults = () => {
+    return (
+        <div>
+            <section className="search__results">
+
+                {/* Navbar */}
+                <Navbar />
+                {/* End of Navbar */}
+
+                
+            </section>
+        </div>
+    )
+}
+
+export default SearchResults;
