29c60de2fc162e655cbb08b23a9a7bb1e66fe854
Abram <israelvictory87@gmail.com>
2022-09-01 13:55:26 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="App">
+
+          <Routes>
+
+            {/* Main Pages */}
+            {/* <Route path="/" component={<Home />} element={<Home />}></Route> */}
+            {/* End of Main Pages */}
+
+          </Routes>
+
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
