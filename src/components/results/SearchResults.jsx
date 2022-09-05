5cd865c5d0938a233e7b974d8eef280857980115
Abram <israelvictory87@gmail.com>
2022-09-05 15:41:49 +0100

    Mapped through anime data

--- src/components/results/SearchResults.jsx
+++ src/components/results/SearchResults.jsx
@@ -1,156 +1,46 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./search_results.css";
-import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 
-// Image imports
-import naruto_movie from "../images/naruto-the-movie.png";
-import naruto_movie_two from "../images/naruto-the-movie-two.png";
-import naruto_movie_three from "../images/naruto-the-movie-three.png";
-import naruto_shippuden from "../images/naruto-shippuden.png";
-import naruto_shippuden_two from "../images/naruto-shippuden-two.png";
-import naruto_shippuden_three from "../images/naruto-shippuden-three.png";
 
+const SearchResults = ({ results }) => {
 
-const SearchResults = () => {
     return (
         <div>
             <section className="search__results">
 
-                {/* Navbar */}
-                <Navbar />
-                {/* End of Navbar */}
-
-
                 <div className="results container">
                     <div className="results__head">
-                        <h2 className="result__title">
+                        <h4 className="result__title">
                             Search <span className="anime__scraper">Results</span>
-                        </h2>
+                        </h4>
                         <p className="h5">These are the results we found bearing the name: “Naruto the movie”</p>
                     </div>
 
                     <div className="results__row row">
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_movie} alt="naruto-the-movie" className="img-responsive" />
+                        {results.map((result) => (
+                            <div className="result__col col-lg-6 col-md-6">
+                                <div className="head">
+                                    <img src={result.image_src} alt={result.image_alt} className="img-responsive" />
+                                </div>
+
+                                <div className="body">
+                                    <h4 className="anime__result__title">
+                                        {result.title}
+                                    </h4>
+
+                                    <p className="anime__result__mute">
+                                        {result.link}
+                                    </p>
+
+                                    <Link to={result.link} className="anime__result__link">
+                                        Click to continue <i className="fa fa-link" aria-hidden="true"></i>
+                                    </Link>
+                                </div>
                             </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto the Movie: Ninja Clash in the Land of Snow (2004)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_movie_two} alt="naruto-the-movie" className="img-responsive" />
-                            </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto the Movie 2: Legend of the Stone of Gelel (2005)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_movie_three} alt="naruto-the-movie" className="img-responsive" />
-                            </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto the Movie 3: Guardians of the Crescent Moon Kingdom (2006)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_shippuden} alt="naruto-the-movie" className="img-responsive" />
-                            </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto Shippûden: The Movie (2007)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_shippuden_two} alt="naruto-the-movie" className="img-responsive" />
-                            </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto Shippuden: The Movie - Bonds (2008)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="result__col col-lg-6 col-md-6">
-                            <div className="head">
-                                <img src={naruto_shippuden_three} alt="naruto-the-movie" className="img-responsive" />
-                            </div>
-
-                            <div className="body">
-                                <h4 className="anime__result__title">
-                                    Naruto Shippûden: The Movie 3: Inheritors of the Will of Fire (2009)
-                                </h4>
-
-                                <p className="anime__result__mute">
-                                    https://www.imdb.com/name/nm0645513/?ref_=ttls_li_dr_0of
-                                </p>
-
-                                <Link to="" className="anime__result__link">
-                                    Click to continue <i className="fa fa-link" aria-hidden="true"></i>
-                                </Link>
-                            </div>
-                        </div>
+                            )
+                        )};
                     </div>
 
                     <div className="result__meta">
