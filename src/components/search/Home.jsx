db4e9c02bb9a5050e6e71ee00ba07466ad577def
Abram <israelvictory87@gmail.com>
2022-09-05 15:42:31 +0100

    Fetch anime by search

--- src/components/search/Home.jsx
+++ src/components/search/Home.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+// Internal Imports
+import React, { useState } from 'react';
+
+// Axios Imports
+import axios from "axios";
+
+// Styles Imports
 import "./main.css";
-import Navbar from '../navbar/Navbar';
+
+// Component Imports
+import Navbar from "../navbar/Navbar";
+import SearchResults from "../results/SearchResults";
 
 
 const Home = () => {
+
+    // set anime state
+    const [anime, setAnime] = useState('');
+    const [results, setAnimeResults] = useState([]);
+
+    // function to handle anime change
+    const handleAnimeChange = (event) => {
+
+        // set anime state to the event value
+        setAnime(event.target.value);
+
+        // clear input value
+        event.target.value = "";
+    };
+
+    // function to search for an anime
+    const AnimeSearch = (event) => {
+
+        // prevent default behaviour of form
+        event.preventDefault();
+
+        // baseURL
+        const baseURL = "https://animesukurepa.up.railway.app/api/v1/";
+
+        axios({
+            method: "POST",
+            url: `${baseURL}search/`,
+            data: {
+                "name": anime,
+            },
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then((res) => {
+
+                // pass anime data to search results
+                let animesJson = res.data.data;
+
+                // set animes data to anime results state
+                setAnimeResults(animesJson);
+            })
+            .catch((err) => {
+                console.log("Erorr: ", err);
+            });
+    };
+
     return (
         <>
 
@@ -25,9 +81,15 @@ const Home = () => {
                             Find and download anime with ease
                         </h4>
 
-                        <form method="post">
+                        <form onSubmit={AnimeSearch}>
                             <div className="form-group">
-                                <input type="text" className="search__input" placeholder="What are you looking for?" />
+                                <input
+                                    type="text"
+                                    className="search__input"
+                                    placeholder="What are you looking for?"
+                                    onChange={handleAnimeChange}
+                                    value={anime}
+                                />
                             </div>
 
                             <div className="form-group">
@@ -43,6 +105,10 @@ const Home = () => {
                 </div>
             </section>
             {/* End of Main */}
+
+            {/* Search Results */}
+            {results.length > 0 ? <SearchResults results={results} /> : "" }
+            {/* End of Search Results */}
         </>
     )
 }
