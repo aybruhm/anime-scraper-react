2d2c4f245bb5a26fd2ff390ccf802aa5a484f62d
Abram <israelvictory87@gmail.com>
2022-09-08 01:30:24 +0100

    Created refreshPage function when nav link is clicked

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,17 @@ import "./navbar_styles.css"
 
 
 const Navbar = () => {
+
+    const refreshPage = () => {
+        window.location.reload(false);
+    }
+
     return (
         <>
             <nav className="navbar">
 
                 <div className="container">
-                    <Link className="navbar-brand me-2 mb-1 nav__link" to="/">
+                    <Link className="navbar-brand me-2 mb-1 nav__link" to="/" onClick={refreshPage}>
                         Anime<span className="anime__scraper">sukurepa</span>
                     </Link>
                 </div>
